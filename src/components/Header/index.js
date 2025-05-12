ec5efd5fc521a190bb9ddd782b704600a749eeb6
Gabrielneves05 <gabrielneves05@outlook.com>
2025-05-12 16:52:26 -0300

    change of project name

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,10 @@
-import React from "react";
 import * as C from "./styles";
 
 const Header = () => {
   return (
     <C.Container>
       <C.Header>
-        <C.Title>Gerenciador Financeiro</C.Title>
+        <C.Title>MoneyTrack</C.Title>
       </C.Header>
     </C.Container>
   );
